cfb245b5ba1a333e161cd39f7db08adffe76a7da
mastak <levprav43@gmail.com>
2026-01-22 09:15:26 +0400

    Update models.py

--- models.py
+++ models.py
@@ -1,55 +1,60 @@
 # models.py
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class Publisher(Base):
     __tablename__ = 'publisher'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    books = relationship("Book", back_populates="publisher")
+    books = relationship('Book', back_populates='publisher')
 
 
 class Book(Base):
     __tablename__ = 'book'
+
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     id_publisher = Column(Integer, ForeignKey('publisher.id'), nullable=False)
 
-    publisher = relationship("Publisher", back_populates="books")
-    stocks = relationship("Stock", back_populates="book")
+    publisher = relationship('Publisher', back_populates='books')
+    stock = relationship('Stock', back_populates='book')
 
 
 class Shop(Base):
     __tablename__ = 'shop'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    stocks = relationship("Stock", back_populates="shop")
+    stock = relationship('Stock', back_populates='shop')
 
 
 class Stock(Base):
     __tablename__ = 'stock'
+
     id = Column(Integer, primary_key=True)
     id_book = Column(Integer, ForeignKey('book.id'), nullable=False)
     id_shop = Column(Integer, ForeignKey('shop.id'), nullable=False)
     count = Column(Integer, nullable=False)
 
-    book = relationship("Book", back_populates="stocks")
-    shop = relationship("Shop", back_populates="stocks")
-    sales = relationship("Sale", back_populates="stock")
+    book = relationship('Book', back_populates='stock')
+    shop = relationship('Shop', back_populates='stock')
+    sales = relationship('Sale', back_populates='stock')
 
 
 class Sale(Base):
     __tablename__ = 'sale'
-    id = Column(Integer, primary_key=True)
-    price = Column(Numeric, nullable=False)
-    date_sale = Column(DateTime, nullable=False)
-    id_stock = Column(Integer, ForeignKey('stock.id'), nullable=False)
-    count = Column(Integer, nullable=False)
 
-    stock = relationship("Stock", back_populates="sales")
+    id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
+    date_sale = Column(Date, nullable=False)
+    id_stock = Column(Integer, ForeignKey('stock.id'), nullable=False)
+    count = Column(Integer, nullable=True)
+
+    stock = relationship('Stock', back_populates='sales')
